762ebb7132778cb12ed828df551dd8bf2174f3d1
groknut <184639205+groknut@users.noreply.github.com>
2025-12-12 17:25:20 +0800

    formate output

--- board.cpp
+++ board.cpp
@@ -20,8 +20,7 @@ void Board::print() const
 
 	int size = grid.size();
 	    
-	    // Заголовок
-	    std::cout << "\n   ";
+	    std::cout << std::endl << "   ";
 	    for (int i = 0; i < size; i++) {
 	        std::cout << std::setw(2) << i << " ";
 	    }
@@ -31,7 +30,6 @@ void Board::print() const
 	    }
 	    std::cout << std::endl;
 	    
-	    // Данные
 	    for (int i = 0; i < size; i++) {
 	        std::cout << std::setw(2) << i << "| ";
 	        for (int j = 0; j < size; j++) {
@@ -40,15 +38,6 @@ void Board::print() const
 	        std::cout << std::endl;
 	    }
 	    std::cout << std::endl;
-	
-	// for (size_t i = 0; i < grid.size(); i++)
-	// {
-	// 	std::cout << i << '|';
-	// 	for (size_t j = 0; j < grid[i].size(); j++)
-	// 		std::cout << grid[i][j] << '|';
-	// 	std::cout << std::endl;
-	// }
-	// std::cout << std::endl;
 }
 
 const std::vector<std::vector<char>>& Board::getGrid() const 
@@ -59,7 +48,7 @@ const std::vector<std::vector<char>>& Board::getGrid() const
 
 bool Board::isCellEmpty(const int& row,const int& col) const 
 {
-	return (size_t)row >= 0 && (size_t)row < grid.size() && (size_t)col >= 0 && (size_t)col < grid.size() && grid[row][col] == empty;
+	return row >= 0 && row < board_size && col >= 0 && col < board_size && grid[row][col] == empty;
 }
 
 bool Board::isFull() const
@@ -78,14 +67,14 @@ int Board::getSize() const
 
 void Board::setCell(const int& y, const int& x, const char& mark)
 {
-	if (y < 0 || (size_t)y > grid.size() || x < 0 || (size_t)x > grid.size())
+	if (y < 0 || y > board_size || x < 0 || x > board_size)
 		throw GridError();
 	grid[y][x] = mark;
 }
 
 const char& Board::getCell(const int& y, const int& x) const
 {
-	if (y < 0 || (size_t)y > grid.size() || x < 0 || (size_t)x > grid.size())
+	if (y < 0 || y > board_size || x < 0 || x > board_size)
 		throw GridError();
 	return grid[y][x];
 }
