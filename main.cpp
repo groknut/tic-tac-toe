16c958fc4739f803b395d02659426a5df42b9600
groknut <184639205+groknut@users.noreply.github.com>
2025-12-18 21:54:17 +0800

    add new method for start game

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@ int main()
 	Cfig config("config.ini", Cfig::EQUAL, Cfig::HASH);
 
 	Game game(config);
-    std::cout << "\n=== Tic-Tac-Toe ===\n";
-    do
-	{
-		game.printBoard();
-		game.handleInput();
- 	} while (!game.isOver());
+
+    game.run();
+
+    // do
+	// {
+	// 	game.printBoard();
+	// 	game.handleInput();
+ 	// } while (!game.isOver());
  	
     return 0;
 }
